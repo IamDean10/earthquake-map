b64463c42bb5b114d1cd0545357883b3807ca08d
Jan Dean <98754979+IamDean10@users.noreply.github.com>
2023-11-12 11:13:36 +0800

    feat: add error handler

--- resources/js/api.js
+++ resources/js/api.js
@@ -3,6 +3,9 @@ async function getEarthquakesFeed() {
   const response = await $.ajax({
     url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson",
     type: "GET",
+    error: function (xhr) {
+      errorHandler(xhr.responseText, true);
+    },
   });
   return response;
 }
@@ -12,6 +15,19 @@ async function getEarthquakesDetail(url) {
   const response = await $.ajax({
     url: url,
     type: "GET",
+    error: function (xhr) {
+      errorHandler(xhr.responseText);
+    },
   });
   return response;
+}
+
+// Handle api errors
+function errorHandler(response, isInitial) {
+  alert(
+    "Please make sure you are connected to the internet. If the issue persists, please contact an administrator.\nAPI Response: " +
+      response
+  );
+  // If api is in initial state reload page
+  isInitial ? window.location.reload() : "";
 }
--- resources/js/utils.js
+++ resources/js/utils.js
@@ -3,7 +3,7 @@ function epochToDate(epoch) {
   if (epoch < 10000000000) epoch *= 1000; // convert to milliseconds (Epoch is usually expressed in seconds, but Javascript uses Milliseconds)
 
   //   Convert epoch to en-US date
-  var date = new Date(epoch).toLocaleDateString("en-US", {
+  const date = new Date(epoch).toLocaleDateString("en-US", {
     weekday: "long",
     year: "numeric",
     month: "long",
